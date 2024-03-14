df2dc51f4b32600080522c6af0601dc6f194f66e
Pratik Pawar <PP7701264@GMAIL.COM>
2024-03-14 05:50:58 +0530

    initial commit
    add files on github

---
+++ home.php
@@ -0,0 +1,46 @@
+<?php 
+
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+
+ ?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>HOME</title>
+
+    <style>
+        body{
+            background-color:white;
+        }
+        </style>
+
+</head>
+
+<body>
+
+     <h1>Hello, <?php echo $_SESSION['name']; ?></h1>
+
+     <a href="logout.php">Logout</a>
+     <a href="pratik.html">START LEARNING</a>
+
+</body>
+
+</html>
+
+<?php 
+
+}else{
+
+     header("Location: index.php");
+
+     exit();
+
+}
+
+ ?>
---
+++ script.js
@@ -0,0 +1,14 @@
+window.onscroll = function {myfunction()};
+
+var header = document.getElementById("myheder");
+var sticky = header.offsetTop;
+
+function myfunction() {
+    if(window.pageYOffset > sticky){
+        header.classList.add('sticky');
+    }
+    else{
+        header.classList.remove("sticky");
+    }
+
+}
